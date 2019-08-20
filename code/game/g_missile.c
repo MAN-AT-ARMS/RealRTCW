0da8c5d2007bbe67d966ab355e71160d5d1ed950
German <karabastov@yahoo.com>
2019-08-20 20:06:55 +0300

    Panzerfaust changed TR_GRAVITY to TR_GRAVITY_LOW

--- code/game/g_missile.c
+++ code/game/g_missile.c
@@ -1127,7 +1127,7 @@ qboolean	isPlayer = (self->client && !self->aiCharacter);	// Knightmare added
 	bolt->clipmask = MASK_MISSILESHOT;
 
 	//bolt->s.pos.trType = TR_LINEAR;
-	bolt->s.pos.trType = TR_GRAVITY; // gothicstein
+	bolt->s.pos.trType = TR_GRAVITY_LOW; // gothicstein
 	bolt->s.pos.trTime = level.time - MISSILE_PRESTEP_TIME;     // move a bit on the very first frame
 	VectorCopy( start, bolt->s.pos.trBase );
 //	VectorScale( dir, 900, bolt->s.pos.trDelta );	// old speed was 900
